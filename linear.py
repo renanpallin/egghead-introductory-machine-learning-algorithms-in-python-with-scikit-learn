eb556c2cce55d593f0be567ff0c85a5ed5dbc326
Renan Pallin <renanpallin@gmail.com>
2017-12-13 18:30:31 -0200

    Linear Regretion

--- linear.py
+++ linear.py
@@ -1,8 +1,28 @@
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 boston = datasets.load_boston()
-print(boston)
+
+X = boston.data
+y = boston.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=16)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+# plt.scatter(y_test, predictions)
+# plt.xlabel("Actual prices")
+# plt.ylabel("Predicted Prices")
+# plt.show()
+
+#r squared, coefficiente of determinition
+print(model.score(X_test, y_test))
+
+#mean squared error
+print(metrics.mean_squared_error(y_test, predictions))
